a0ba598b67179dbc75b937c8174dc4fcd4de09a2
ConnorCrwfNRG <cdcrwf@utexas.edu>
2020-12-21 22:24:27 -0600

    catkinized package

--- config/simulator.lua
+++ config/simulator.lua
@@ -10,7 +10,7 @@ function DegToRad(d)
   return math.pi * d / 180
 end
 
-map_name =  "maps/GDC1/GDC1.vectormap.txt";
+map_name =  "/home/connor/ar_ws/src/amrl_libraries/amrl_maps/GDC1/GDC1.vectormap.txt";
 -- Simulator starting location.
 start_x = -21
 start_y = 9.0
